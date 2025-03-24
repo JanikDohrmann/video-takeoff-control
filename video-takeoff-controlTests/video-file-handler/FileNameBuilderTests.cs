cc5f097684e7a4aecefbe6b52e54e088c7875926
Janik Dohrmann <58031983+JanikDohrmann@users.noreply.github.com>
2025-03-24 14:04:12 +0100

    restructured tests

---
+++ video-takeoff-controlTests/video-file-handler/FileNameBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using video_takeoff_control.video_file_handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace video_takeoff_control.video_file_handler.Tests
+{
+    [TestClass()]
+    public class FileNameBuilderTests
+    {
+        [TestMethod()]
+        public void buildFileNameTest()
+        {
+            string actualFileName = FileNameBuilder.buildFileName("", "Weisprung Männer");
+            Assert.IsTrue(Regex.IsMatch(actualFileName, @"Video_[äöüa-zA-Z-]+_[T\d-]+.avi"));
+        }
+    }
+}
