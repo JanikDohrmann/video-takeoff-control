ffa2550baa8ae0dfc9af0aa0bf210c6a3aecc33f
Janik Dohrmann <58031983+JanikDohrmann@users.noreply.github.com>
2025-06-04 22:43:36 +0200

    Fixed test

--- video-takeoff-controlTests/video-file-handler/FileNameBuilderTests.cs
+++ video-takeoff-controlTests/video-file-handler/FileNameBuilderTests.cs
@@ -15,8 +15,8 @@ namespace video_takeoff_control.video_file_handler.Tests
         [TestMethod()]
         public void buildFileNameTest()
         {
-            string actualFileName = FileNameBuilder.buildFileName("", "Weisprung Männer");
-            Assert.IsTrue(Regex.IsMatch(actualFileName, @"Video_[äöüa-zA-Z-]+_[T\d-]+.avi"));
+            string actualFileName = FileNameBuilder.buildFileName("", "Weisprung Männer", 0);
+            Assert.IsTrue(Regex.IsMatch(actualFileName, @"0_Video_[äöüa-zA-Z-]+_[T\d-]+.avi"));
         }
     }
 }
