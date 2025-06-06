ddba8d50c36c79b9ba82bf3d9747fef5adb77cfd
Janik Dohrmann <58031983+JanikDohrmann@users.noreply.github.com>
2025-06-06 22:02:44 +0200

    Change timestamp in videon file name to use 24 hour format

--- video-takeoff-control/video-file-handler/FileNameBuilder.cs
+++ video-takeoff-control/video-file-handler/FileNameBuilder.cs
@@ -11,7 +11,7 @@ namespace video_takeoff_control.video_file_handler
     {
         public static string buildFileName(string folderpath, string competitionName, int attemptNumber)
         {
-            string filename = Path.Combine(folderpath, competitionName, string.Format("{0}_Video_{1}_{2}.avi", attemptNumber, competitionName.Replace(" ", "-"), DateTime.Now.ToString("yyyy'-'MM'-'dd'T'hhmmss")));
+            string filename = Path.Combine(folderpath, competitionName, string.Format("{0}_Video_{1}_{2}.avi", attemptNumber, competitionName.Replace(" ", "-"), DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HHmmss")));
 
             return filename;
         }
